e79f23e4d7d34e35e0cd0536f0e79032471660ec
vethusonamitAlgonquin <amit0004algonquinlive.com>
2024-04-12 16:11:07 -0400

    part2 was completed

---
+++ infra/tf-app/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.96.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "amit0004githubactions"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
+    # use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  #   use_oidc = true
+}
